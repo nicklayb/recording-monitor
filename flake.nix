fde4968b0d5c52751775082709aa4b5de00cb209
nicklayb <nicklay@me.com>
2026-06-18 14:07:13 -0400

    Clean flake

--- flake.nix
+++ flake.nix
@@ -29,36 +29,32 @@
           '';
         };
 
-        packages = {
-          recording-monitor = pkgs.buildNpmPackage {
-            pname = "recording-monitor";
-            version = "1.0.0";
+        packages.default = pkgs.buildNpmPackage {
+          pname = "recording-monitor";
+          version = "1.0.0";
 
-            src = ./.;
+          src = ./.;
 
-            npmDepsHash = "sha256-9BA8yQ22rM6Be13iu/FqPQQ8oGUbDBzp6VBrHOXhPNo=";
-            dontNpmBuild = true;
+          npmDepsHash = "sha256-9BA8yQ22rM6Be13iu/FqPQQ8oGUbDBzp6VBrHOXhPNo=";
+          dontNpmBuild = true;
 
-            installPhase = ''
-              runHook preInstall
+          installPhase = ''
+            runHook preInstall
 
-              mkdir -p $out/bin
+            mkdir -p $out/bin
 
-              cat > $out/bin/recording-monitor <<EOF
-              #!${pkgs.bash}/bin/bash
-              exec ${pkgs.nodejs}/bin/node $out/app/src/index.js
-              EOF
+            cat > $out/bin/recording-monitor <<EOF
+            #!${pkgs.bash}/bin/bash
+            exec ${pkgs.nodejs}/bin/node $out/app/src/index.js
+            EOF
 
-              chmod +x $out/bin/recording-monitor
+            chmod +x $out/bin/recording-monitor
 
-              mkdir -p $out/app
-              cp -R src package.json node_modules $out/app
+            mkdir -p $out/app
+            cp -R src package.json node_modules $out/app
 
-              runHook postInstall
-            '';
-          };
-
-          default = self.packages.${system}.recording-monitor;
+            runHook postInstall
+          '';
         };
 
         darwinModules.default =
